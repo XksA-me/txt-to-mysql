afa02ead416767e6a83b1fcff9db0f3ad342b00d
xksa <pythonbrief@163.com>
2022-01-21 13:28:19 +0800

    完成GUI程序
    Signed-off-by: xksa <pythonbrief@163.com>

--- python-Jonny/txt_to_sql_gui.py
+++ python-Jonny/txt_to_sql_gui.py
@@ -1,15 +1,30 @@
 # 写个GUI
 import PySimpleGUI as sg
+from txt_to_sql import txt_to_sql
+import json
+
+
+# 设置GUI布局
 layout = [
-    [sg.Text('一 句话概括Python')],
-    [sg.Input(key='')],
-    [sg.Button('确认'), sg.Button('取消')]
+    [sg.Text('读取指定文件内容，处理后存入指定数据库表中～')],
+    [sg.FileBrowse('点击选取文件', key='filepath', target='file'), sg.Text(key='file')],
+    [sg.Text('登录用户名'), sg.InputText(key='user', default_text='root', )],
+    [sg.Text('登录密码'), sg.InputText(key='password', default_text='Zjh!1997')],
+    [sg.Text('数据库名称'), sg.InputText(key='database', default_text='sql_study')],
+    [sg.Text('存储的表名'), sg.InputText(key='table', default_text='ctd')],
+
+    [sg.Button('开始处理'), sg.Button('退出')]
 ]
-window = sg.Window('Txt To MySQL', layout)
+
+# 创建窗口程序
+window = sg.Window('Txt To MySQL', layout, default_element_size=(100,))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    if event in (None, '取消'):
+    event, values = window.read()  # 获取数据
+    # print(event)
+    if event=='开始处理':
+        # 将输入数据传入数据处理程序
+        txt_to_sql(values['filepath'], values['user'], values['password'], values['database'], values['table'])
+    else:
+        # event in (None, '退出'):  # 点击退出 关闭程序
         break
 window.close()
